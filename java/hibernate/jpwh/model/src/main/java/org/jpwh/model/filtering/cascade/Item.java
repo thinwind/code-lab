13764b43c52dc31e7cfe07c6887171ca4e0f57f7
Shang <niceshang@icloud.com>
2020-10-20 16:20:58 +0800

    提交JPWH代码

---
+++ java/hibernate/jpwh/model/src/main/java/org/jpwh/model/filtering/cascade/Item.java
@@ -0,0 +1,87 @@
+package org.jpwh.model.filtering.cascade;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    protected Long id;
+
+    @NotNull
+    protected String name;
+
+    @OneToMany(
+        mappedBy = "item",
+        cascade = {CascadeType.DETACH, CascadeType.MERGE}
+    )
+    protected Set<Bid> bids = new HashSet<Bid>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SELLER_ID", nullable = false)
+    @org.hibernate.annotations.Cascade(
+        org.hibernate.annotations.CascadeType.REPLICATE
+    )
+    protected User seller;
+
+    protected String someProp;
+
+    protected Item() {
+    }
+
+    public Item(String name, User seller) {
+        this.name = name;
+        this.seller = seller;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Bid> getBids() {
+        return bids;
+    }
+
+    public void setBids(Set<Bid> bids) {
+        this.bids = bids;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public String getSomeProp() {
+        return someProp;
+    }
+
+    public void setSomeProp(String someProp) {
+        this.someProp = someProp;
+    }
+
+
+    // ...
+}
