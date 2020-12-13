56a538171d852861c52d9c2a938042dbe2af1a84
Shang <niceshang@icloud.com>
2020-12-13 20:17:45 +0800

    add tests

---
+++ java/nio/nio-demo/src/main/java/me/shangyh/codelab/nio/Byte2CharBufferTest.java
@@ -0,0 +1,51 @@
+/* 
+ * Copyright 2020 Shang Yehua
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.shangyh.codelab.nio;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * TODO Byte2CharBufferTest说明
+ *
+ * @author Shang Yehua <niceshang@outlook.com>
+ * @since 2020-11-06  10:18
+ *
+ */
+public class Byte2CharBufferTest {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.out.println("default charset:"+Charset.defaultCharset().name());
+        byte[] something = "十年辛苦不寻常".getBytes("utf-16BE");
+        ByteBuffer byteBuffer = ByteBuffer.wrap(something);
+        System.out.println("byte buffer impl name:"+byteBuffer.getClass().getName());
+        
+        CharBuffer charBuffer = byteBuffer.asCharBuffer();
+        System.out.println("char buffer impl name:"+charBuffer.getClass().getName());
+                
+        System.out.println("byte buffer: position="+byteBuffer.position()+"\tcapacity="+byteBuffer.capacity()+
+                "\tlimit="+byteBuffer.limit());
+                
+        System.out.println("char buffer: position="+charBuffer.position()+"\tcapacity="+charBuffer.capacity()+
+                "\tlimit="+charBuffer.limit());
+        
+        for (int i = 0; i < charBuffer.capacity(); i++) {
+            System.out.print(charBuffer.get()+" ");
+        }
+    }
+}
