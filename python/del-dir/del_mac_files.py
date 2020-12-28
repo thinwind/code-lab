e51a335412674a6ae528a3c0da23fd1b541b3622
Yehua Shang <niceshang@outlook.com>
2020-12-28 20:23:09 +0800

    添加删除mac系统自动生成文件

--- python/del-dir/del_mac_files.py
+++ python/del-dir/del_mac_files.py
@@ -4,13 +4,13 @@
 @Author :  Shang Yehua
 @Email  :  niceshang@outlook.com
 @Desc   :  
-        删除$开头的目录
+        删除mac系统自动生成的文件
 '''
 
 import os
 import shutil
 
-target_dir = "C:\\Users\\Shangyh\\.m2"
+target_dir = "C:\\Users\\Shangyh\\BookLib"
 base_path = os.path.abspath(target_dir) 
 
 def def_dir(dir):
@@ -20,27 +20,32 @@ def print_dir(dir):
     subs = os.listdir(dir)
     parent_path = os.path.abspath(dir)
     for sub in subs:
-        if sub.startswith('$'):
+        if sub.startswith('.'):
             print(sub)
         sub = parent_path+"/"+sub
         if os.path.exists(sub) and os.path.isdir(sub):
             print_dir(sub)
             
-def rm_doller_dir(dir):
+def rm_mac_files(dir):
     subs = os.listdir(dir)
     parent_path = os.path.abspath(dir)
     for sub in subs:
         sub_path = parent_path+"/"+sub
-        if sub.startswith('$'):
-            if os.path.isdir(sub_path):
-                shutil.rmtree(sub_path)
-                # print(sub_path)
+        if sub.startswith('.'):
+            if sub.startswith(".git"):
+                # shutil.rmtree(sub_path)
+                print("git://"+sub_path)
+                # .git dir
+                pass
             else:
-                os.remove(sub_path)
+                if os.path.isdir(sub_path):
+                    shutil.rmtree(sub_path)
+                else :
+                    os.remove(sub_path)
                 # print(sub_path)
         else:
             if os.path.isdir(sub_path):
-                rm_doller_dir(sub_path)
+                rm_mac_files(sub_path)
 
 if __name__ == "__main__":
     print_dir(base_path)
