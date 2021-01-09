1a27a1df110e2a0d078a5d106b1890ffa323eda5
Shang <niceshang@icloud.com>
2021-01-09 18:15:25 +0800

    test quickstart

--- java/rocketmq/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ java/rocketmq/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -32,7 +32,8 @@ public class Producer {
          * Instantiate with a producer group name.
          */
         DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
-
+        producer.setNamesrvAddr("localhost:9876");
+        producer.buildMQClientId();
         /*
          * Specify name server addresses.
          * <p/>
@@ -50,7 +51,7 @@ public class Producer {
          */
         producer.start();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             try {
 
                 /*
