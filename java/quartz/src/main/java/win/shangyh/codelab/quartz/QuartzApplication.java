3d0c46e77fb8bce369b5b9d15de2a44fb5006713
Yehua Shang <niceshang@outlook.com>
2021-01-22 16:44:08 +0800

    添加quartz配置

--- java/quartz/src/main/java/win/shangyh/codelab/quartz/QuartzApplication.java
+++ java/quartz/src/main/java/win/shangyh/codelab/quartz/QuartzApplication.java
@@ -1,13 +1,18 @@
 package win.shangyh.codelab.quartz;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuartzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzApplication.class, args);
-	}
+  }
 
 }
