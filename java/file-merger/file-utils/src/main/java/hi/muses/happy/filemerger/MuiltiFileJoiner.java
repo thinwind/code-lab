d0d5b59f66c24e56857b8aa2ad5590688fa51688
Shang <niceshang@icloud.com>
2021-01-29 07:35:52 +0800

    add joiner

--- java/file-merger/file-utils/src/main/java/hi/muses/happy/filemerger/MuiltiFileJoiner.java
+++ java/file-merger/file-utils/src/main/java/hi/muses/happy/filemerger/MuiltiFileJoiner.java
@@ -13,9 +13,11 @@
  */
 package hi.muses.happy.filemerger;
 
+import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 
 /**
@@ -28,31 +30,79 @@ import java.io.Reader;
  */
 public class MuiltiFileJoiner {
     private final String[] files;
-    private final Reader[] readers;
-    private final int bufferSize;
+    private final BufferedReader[] readers;
     private final String[] bufferLines;
+    private final boolean[] readerEnded;
     private boolean end;
+    private final LineMatcher lineMatcher;
 
-    public MuiltiFileJoiner(String[] files,int bufferSize) throws FileNotFoundException {
+    public MuiltiFileJoiner(String[] files, LineMatcher lineMatcher) throws FileNotFoundException {
         this.files = files;
-        this.bufferSize = bufferSize;
-        readers = new Reader[files.length];
+        readers = new BufferedReader[files.length];
         bufferLines = new String[files.length];
+        readerEnded = new boolean[files.length];
+        end = false;
+        this.lineMatcher = lineMatcher;
         for (int i = 0; i < files.length; i++) {
             readers[i] = new BufferedReader(new FileReader(files[i]));
         }
     }
-    
-    public boolean
-    
+
+    public String nextLine() throws IOException {
+        String tmpLine = readers[0].readLine();
+        if ( tmpLine == null) {
+            closeReaderSilently(readers[0]);
+            end=true;
+            return null;
+        }
+        bufferLines[0] = tmpLine;
+        
+        for (int i = 1; i < readers.length; i++) {
+            tmpLine = null;
+            if (bufferLines[i] != null) {
+                continue;
+            }
+            if(!readerEnded[i]){
+                tmpLine = readers[i].readLine();
+                if (tmpLine == null) {
+                    readerEnded[i]=true;
+                    closeReaderSilently(readers[i]);
+                    continue;
+                }
+                bufferLines[i]=tmpLine;
+            }
+        }
+        
+        MatchResult result = lineMatcher.matchLines(Arrays.copyOf(bufferLines, bufferLines.length));
+        boolean[] matchedDetails = result.getMatchedDetails();
+        for (int i = 0; i < matchedDetails.length; i++) {
+            if(matchedDetails[i]){
+                bufferLines[i+1] = null;
+            }
+        }
+        return result.getJoinedLine();
+    }
+
+    public boolean isEnd() {
+        return end;
+    }
+
     public void complete() {
         for (Reader reader : readers) {
-            try {
-                reader.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            closeReaderSilently(reader);
         }
     }
 
+    private void closeReaderSilently(Reader reader){
+        try {
+            reader.close();
+        } catch (Exception e) {
+            //just print the stack and do nothing
+            e.printStackTrace();
+        }
+    }
+
+    public String[] getFiles() {
+        return files.clone();
+    }
 }
