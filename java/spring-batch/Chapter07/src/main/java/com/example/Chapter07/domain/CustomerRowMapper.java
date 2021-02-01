d04606db62b99f4fbe1207901289f50523adf88e
Yehua Shang <niceshang@outlook.com>
2021-02-01 18:30:25 +0800

    fix-width test

--- java/spring-batch/Chapter07/src/main/java/com/example/Chapter07/domain/CustomerRowMapper.java
+++ java/spring-batch/Chapter07/src/main/java/com/example/Chapter07/domain/CustomerRowMapper.java
@@ -23,22 +23,22 @@ import org.springframework.jdbc.core.RowMapper;
 /**
  * @author Michael Minella
  */
-public class CustomerRowMapper implements RowMapper<Customer> {
+// public class CustomerRowMapper implements RowMapper<Customer> {
 
-	@Override
-	public Customer mapRow(ResultSet resultSet, int rowNumber) throws
-			SQLException {
-		Customer customer = new Customer();
+// 	@Override
+// 	public Customer mapRow(ResultSet resultSet, int rowNumber) throws
+// 			SQLException {
+// 		Customer customer = new Customer();
 
-		customer.setId(resultSet.getLong("id"));
-		customer.setAddress(resultSet.getString("address"));
-		customer.setCity(resultSet.getString("city"));
-		customer.setFirstName(resultSet.getString("firstName"));
-		customer.setLastName(resultSet.getString("lastName"));
-		customer.setMiddleInitial(resultSet.getString("middleInitial"));
-		customer.setState(resultSet.getString("state"));
-		customer.setZipCode(resultSet.getString("zipCode"));
+// 		customer.setId(resultSet.getLong("id"));
+// 		customer.setAddress(resultSet.getString("address"));
+// 		customer.setCity(resultSet.getString("city"));
+// 		customer.setFirstName(resultSet.getString("firstName"));
+// 		customer.setLastName(resultSet.getString("lastName"));
+// 		customer.setMiddleInitial(resultSet.getString("middleInitial"));
+// 		customer.setState(resultSet.getString("state"));
+// 		customer.setZipCode(resultSet.getString("zipCode"));
 
-		return customer;
-	}
-}
+// 		return customer;
+// 	}
+// }
