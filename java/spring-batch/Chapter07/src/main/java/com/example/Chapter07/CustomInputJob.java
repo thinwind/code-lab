2855200a5c5b211ca92909858bb541a165477c0d
Yehua Shang <niceshang@outlook.com>
2021-02-01 18:20:47 +0800

    ch07数据库连接修改

--- java/spring-batch/Chapter07/src/main/java/com/example/Chapter07/CustomInputJob.java
+++ java/spring-batch/Chapter07/src/main/java/com/example/Chapter07/CustomInputJob.java
@@ -1,66 +1,66 @@
-package com.example.Chapter07;
+// package com.example.Chapter07;
 
-import com.example.Chapter07.batch.CustomerItemListener;
-import com.example.Chapter07.batch.CustomerItemReader;
-import com.example.Chapter07.domain.Customer;
+// import com.example.Chapter07.batch.CustomerItemListener;
+// import com.example.Chapter07.batch.CustomerItemReader;
+// import com.example.Chapter07.domain.Customer;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.batch.core.Job;
+// import org.springframework.batch.core.Step;
+// import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+// import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+// import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+// import org.springframework.batch.item.ItemWriter;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
 
-@EnableBatchProcessing
-@SpringBootApplication
-public class CustomInputJob {
+// @EnableBatchProcessing
+// @SpringBootApplication
+// public class CustomInputJob {
 
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+// 	@Autowired
+// 	private JobBuilderFactory jobBuilderFactory;
 
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
+// 	@Autowired
+// 	private StepBuilderFactory stepBuilderFactory;
 
-	@Bean
-	public CustomerItemReader customerItemReader() {
-		CustomerItemReader customerItemReader = new CustomerItemReader();
+// 	@Bean
+// 	public CustomerItemReader customerItemReader() {
+// 		CustomerItemReader customerItemReader = new CustomerItemReader();
 
-		customerItemReader.setName("customerItemReader");
+// 		customerItemReader.setName("customerItemReader");
 
-		return customerItemReader;
-	}
+// 		return customerItemReader;
+// 	}
 
-	@Bean
-	public ItemWriter<Customer> itemWriter() {
-		return (items) -> items.forEach(System.out::println);
-	}
+// 	@Bean
+// 	public ItemWriter<Customer> itemWriter() {
+// 		return (items) -> items.forEach(System.out::println);
+// 	}
 
-	@Bean
-	public Step copyFileStep() {
-		return this.stepBuilderFactory.get("copyFileStep")
-				.<Customer, Customer>chunk(10)
-				.reader(customerItemReader())
-				.writer(itemWriter())
-				.listener(new CustomerItemListener())
-				.build();
-	}
+// 	@Bean
+// 	public Step copyFileStep() {
+// 		return this.stepBuilderFactory.get("copyFileStep")
+// 				.<Customer, Customer>chunk(10)
+// 				.reader(customerItemReader())
+// 				.writer(itemWriter())
+// 				.listener(new CustomerItemListener())
+// 				.build();
+// 	}
 
-	@Bean
-	public Job job() {
-		return this.jobBuilderFactory.get("job")
-				.start(copyFileStep())
-				.build();
-	}
+// 	@Bean
+// 	public Job job() {
+// 		return this.jobBuilderFactory.get("job")
+// 				.start(copyFileStep())
+// 				.build();
+// 	}
 
 
-	public static void main(String[] args) {
+// 	public static void main(String[] args) {
 
-		SpringApplication.run(CustomInputJob.class, args);
-	}
+// 		SpringApplication.run(CustomInputJob.class, args);
+// 	}
 
-}
+// }
 
