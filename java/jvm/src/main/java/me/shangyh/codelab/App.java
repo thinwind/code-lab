1c7e11c977b6793ead40d77719e10f061426cb81
Yehua Shang <niceshang@outlook.com>
2021-03-28 10:29:27 +0800

    add jvm code

---
+++ java/jvm/src/main/java/me/shangyh/codelab/App.java
@@ -0,0 +1,13 @@
+package me.shangyh.codelab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
