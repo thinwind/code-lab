e812186b9084c6797c5c525b644c9c9b7bc6f41d
Yehua Shang <niceshang@outlook.com>
2021-04-22 21:34:19 +0800

    两数相加over

--- leetcode/add_two_num.py
+++ leetcode/add_two_num.py
@@ -11,6 +11,11 @@
 #         self.val = val
 #         self.next = next
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         root = ListNode()
@@ -45,4 +50,22 @@ class Solution:
             result.next = None
         
         return root.next
+
+if __name__ == "__main__":
+    l1 = ListNode()
+    l1.val = 2
+    l1.next = ListNode()
+    l1.next.val = 4
+    l1.next.next = ListNode()
+    l1.next.next.val = 3
+    
+    l2 = ListNode()
+    l2.val = 5
+    l2.next = ListNode()
+    l2.next.val = 6
+    l2.next.next = ListNode()
+    l2.next.next.val = 4
+    
+    r = Solution().addTwoNumbers(l1,l2)
+    print(r)
 # @lc code=end
