989954728584237d95ccf56e19778b3d0067e5d4
Shang <niceshang@icloud.com>
2021-07-22 09:05:23 +0800

    删除eclipse文件的功能

---
+++ python/del-dir/del_eclipse_files.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+@Time   :  2021-7月-22 08:47
+@Author :  Shang Yehua
+@Email  :  niceshang@outlook.com
+@Desc   :  
+        删除eclipse生成的文件，清理java工作区
+'''
+import os
+import shutil
+
+target_files=['.classpath','.project','.settings','.vscode','.factorypath']
+
+def clean_eclipse_file(wp):
+    subs = os.listdir(wp)
+    parent_path = os.path.abspath(wp)
+    for sub in subs:
+        sub_path = parent_path+"/"+sub
+        if sub in target_files:
+            if os.path.isdir(sub_path):
+                shutil.rmtree(sub_path)
+            else:
+                os.remove(sub_path)
+        else:
+            if os.path.isdir(sub_path):
+                clean_eclipse_file(sub_path)
+
+if __name__ == '__main__':
+    wp = '/Users/shangyehua/Myworkspace/learn-demo/batch-demo'
+    clean_eclipse_file(wp)
