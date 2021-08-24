6d736657955266dd65fc5bf62bfaf80f8a5ce107
Shang <niceshang@icloud.com>
2021-08-24 14:57:17 +0800

    文件排序和比较

---
+++ python/algs/sort-diff/file_diff.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+'''
+@Time   :  2021-8月-24 11:38
+@Author :  Shang Yehua
+@Email  :  niceshang@outlook.com
+@Desc   :  
+        找到文件不同
+'''
+
+from pathlib import Path
+
+def find_diff(left,right):
+    with open(left,'rt') as left_file,open(right,'rt') as right_file,\
+        open(Path(".") / f"out/{left}.only",'wt') as left_only,open(Path(".") / f"out/{right}.only",'wt') as right_only:
+        left_line = left_file.readline()
+        right_line = right_file.readline()
+        while True:
+            #判断是否结束
+            if not left_line:
+                # 将右侧写出
+                while right_line:
+                    right_only.write(right_line)
+                    right_line = right_file.readline()
+                return
+            if not right_line:
+                while left_line:
+                    left_only.write(left_line)
+                    left_line = left_file.readline()
+                return
+            
+            if left_line < right_line:
+                left_only.write(left_line)
+                left_line = left_file.readline()
+            elif left_line == right_line or left_line.strip() == right_line.strip():
+                left_line = left_file.readline()
+                right_line = right_file.readline()
+            else:
+                right_only.write(right_line)
+                right_line = right_file.readline()
+                
+
+            
+if __name__ == "__main__":
+    left = Path(".") / "left.txt"
+    right = Path(".") / "right.txt"
+    find_diff(left,right)
---
+++ python/algs/sort-diff/file_sort.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+'''
+@Time   :  2021-8月-24 10:21
+@Author :  Shang Yehua
+@Email  :  niceshang@outlook.com
+@Desc   :  
+        文件排序
+'''
+
+from pathlib import Path
+
+
+def split_file(file_path, block_size):
+    cnt = 1  # 文件数
+    files = []  # 小文件目录
+    with open(file_path, 'rt', encoding='utf-8') as f:
+        while True:
+            lines = f.readlines(block_size)
+            if not lines:
+                break
+            lines.sort()  # 排序
+            files.append(save_file(lines, cnt))
+            cnt += 1
+        return files
+
+
+def save_file(lines, cnt):
+    path = Path(".") / "tmp"
+    filepath = path / f"part.{cnt}"
+    info = ''.join(lines)
+    with open(filepath, "a+t", encoding="utf-8") as f:
+        f.write(f"{info}")
+
+    return filepath
+
+def merge_file(files,file_name):
+    lines = {}  # 用来记录每一路当前最小值
+    fs = [open(_f) for _f in files]
+    out = open(Path(".") / f"out/{file_name}.sorted", "a+")
+    for n,f in zip(range(0,len(fs)),fs):
+        line = f.readline()
+        if line:
+            lines[n] = line
+    
+    while lines:
+        n,l = min(lines.items(), key=lambda x:x[1])
+        out.write(l)
+        next_line = fs[n].readline()
+        if next_line:
+            lines[n] = next_line
+        else:
+            del lines[n]
+            
+    out.close()
+    for f in fs:
+        f.close()
+            
+if __name__ == "__main__":
+    # 每行单个数字
+    file_path = Path(".") / "test.txt"
+    block_size = 20000
+    tmp_files = split_file(file_path,block_size)
+    merge_file(tmp_files,"test.txt")
