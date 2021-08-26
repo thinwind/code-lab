84e69f5b791a62b85ee794e2ac2c888265fd8f8c
Shang <niceshang@icloud.com>
2021-08-26 10:00:41 +0800

    分段排序

--- python/algs/sort-diff/two_phrase_sort.py
+++ python/algs/sort-diff/two_phrase_sort.py
@@ -14,53 +14,107 @@ def split_file(file, block_size):
     cnt = 1  # 文件数
     files = []  # 小文件目录
     cache_size = 200
-    
+    readLines = 0
+    out_file_path = Path(".") / "tmp"/"split" / f"{file}.split.{cnt}"
+    out_file = open(out_file_path, 'wt')
+    files.append(out_file_path)
+
     with open(file, 'rt', encoding='utf-8') as f:
         while True:
-            lines = f.readlines(block_size)
+            if readLines >= block_size:
+                out_file.close()
+                cnt += 1
+                readLines = 0
+                new_file = Path(".") / "tmp"/"split" / f"{file}.split.{cnt}"
+                out_file = open(new_file, 'wt')
+                files.append(new_file)
+            lines = f.readlines(cache_size)
+            readLines += len(lines)
             if not lines:
                 break
-            if sort:
-                lines.sort()  # 排序
-            files.append(save_file(lines, cnt))
-            cnt += 1
+            out_file.writelines(lines)
+
+        out_file.close()
+
         return files
 
 
-def save_file(lines, cnt):
-    path = Path(".") / "tmp"
-    filepath = path / f"part.{cnt}"
-    info = ''.join(lines)
-    with open(filepath, "a+t", encoding="utf-8") as f:
-        f.write(f"{info}")
+def split_and_sort_file(file_path, block_size):
+    cnt = 1  # 文件数
+    files = []  # 小文件目录
+    lines = []
+    cache_size = 50
+    cache_lines = None
+    with open(file_path, 'rt', encoding='utf-8') as f:
+        while True:
+            cache_lines = f.readlines(cache_size)
+            if not cache_lines:
+                if lines:
+                    lines.sort()
+                    files.append(save_file(file_path.name,lines, cnt))
+                break
 
-    return filepath
+            lines += cache_lines
 
-def merge_file(files,file_name):
+            if len(lines) >= block_size:
+                files.append(save_file(file_path.name,lines, cnt))
+                cnt += 1
+                lines = []
+        return files
+
+def save_file(file_name,lines,cnt):
+    lines.sort()  # 排序
+    part_file = Path(".") / "tmp"/"sort" /f"{file_name}.part.{cnt}"
+    with open(part_file, "wt", encoding="utf-8") as f:
+        f.writelines(lines)
+    return part_file
+    
+
+def merge_file(files, file_name):
     lines = {}  # 用来记录每一路当前最小值
     fs = [open(_f) for _f in files]
-    out = open(Path(".") / f"out/{file_name}.sorted", "a+")
-    for n,f in zip(range(0,len(fs)),fs):
+    out = open(file_name, "wt")
+    for n, f in zip(range(0, len(fs)), fs):
         line = f.readline()
         if line:
             lines[n] = line
-    
+
     while lines:
-        n,l = min(lines.items(), key=lambda x:x[1])
+        n, l = min(lines.items(), key=lambda x: x[1])
         out.write(l)
         next_line = fs[n].readline()
         if next_line:
             lines[n] = next_line
         else:
             del lines[n]
-            
+
     out.close()
     for f in fs:
         f.close()
-            
+
+
 if __name__ == "__main__":
-    # 每行单个数字
     file_path = Path(".") / "test.txt"
-    block_size = 20000
-    tmp_files = split_file(file_path,block_size)
-    merge_file(tmp_files,"test.txt")
+    tmp_sort = Path('.') / "tmp" / "sort"
+    if not tmp_sort.exists():
+        tmp_sort.mkdir()
+    
+    tmp_split = Path('.') / "tmp" / "split"
+
+    if not tmp_split.exists():
+        tmp_split.mkdir()
+
+    tmp_merge = Path('.') / "tmp" / "merge"
+    if not tmp_merge.exists():
+        tmp_merge.mkdir()
+
+    sub_files = split_file(file_path, 20000)
+
+    sorted_subfiles = []
+    for sub_file in sub_files:
+        tmpfiles = split_and_sort_file(sub_file, 1000)
+        sorted_subfile = Path(".") / "tmp" / "merge" / f"{sub_file.name}.merge"
+        merge_file(tmpfiles, sorted_subfile)
+        sorted_subfiles.append(sorted_subfile)
+    sorted_file = Path(".") / "out" / f"{file_path}.sorted"
+    merge_file(sorted_subfiles, sorted_file)
