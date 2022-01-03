983b6bf28fff2a7fd46f83bfe396b745640dc996
Shang <niceshang@icloud.com>
2022-01-03 22:52:46 +0800

    add jinja

--- java/springboot-demo/src/main/java/io/github/deergate/demos/DemoApplication.java
+++ java/springboot-demo/src/main/java/io/github/deergate/demos/DemoApplication.java
@@ -1,7 +1,10 @@
 package io.github.deergate.demos;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -10,5 +13,12 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(@Value("${test.val:}") String val) {
+        return args -> {
+            System.out.println(val);
+        };
     }
 }
