7c6a3e7edfc1fad4b11d3954ab117f3fd6cf83f2
Shang <niceshang@icloud.com>
2022-01-18 15:39:18 +0800

    修改cups模板

--- python/jinja-demo/quick_start.py
+++ python/jinja-demo/quick_start.py
@@ -8,16 +8,19 @@
 '''
 
 from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
 
 env = Environment(
     loader=FileSystemLoader("templates")
 )
 
+output_path = Path(".") / "output"
+
 def cups_json_render():
     template_name = "cups8583.json.jinja"
     template = env.get_template(template_name)
     ctx={
-        "length_size":4,
+        "length_size":2,
         "length_size_included":"true",
         "header_enabled":"true",
         "header_key":"header",
@@ -26,10 +29,25 @@ def cups_json_render():
             ("Header Length","头长度",1,"b"),
             ("Header Flag and Version","头标识和版本号",1,"b"),
             ("Total Message Length","报文总长度",4,"n"),
+            ("Destination Station ID","目的ID",11,"ans"),
+            ("Source Station ID","源ID",11,"ans"),
+            ("Reserved for Use","保留使用",3,"n"),
+            ("Batch Number","批次号",1,"b"),
+            ("Transaction Information","交易信息",8,"ans"),
+            ("User Information","用户信息",1,"b"),
+            ("Reject Code","拒绝码",5,"b")
         ]
     }
-    result = template.render(ctx)
-    print(result)
+    total_length = 0
+    for comp in ctx["header_components"]:
+        total_length += comp[2]
+    if not total_length == ctx["header_length"]:
+        raise AssertionError
+    result = template.generate(ctx)
+
+    with open(output_path / "cups8583.json", "w",encoding="utf-8") as output_file:
+        for str in result:
+            output_file.write(str)
     
     
 if __name__ == "__main__":
