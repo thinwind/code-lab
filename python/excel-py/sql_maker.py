0382669fe6d524c5ab36b1a7761ae177323a305c
Shang Yehua <niceshang@outlook.com>
2023-09-13 18:11:37 +0800

    生成中

--- python/excel-py/sql_maker.py
+++ python/excel-py/sql_maker.py
@@ -31,9 +31,10 @@ class ColumnDef:
     def __init__(
         self,
         column_name,
+        column_title_zh,
         column_type,
         column_length,
-        column_null,
+        column_not_null,
         column_default,
         column_description,
         column_range,
@@ -42,11 +43,12 @@ class ColumnDef:
         in_general_ledger,
     ):
         self.column_name = column_name
+        self.column_title_zh = column_title_zh
         self.column_type = column_type
         self.column_length = column_length
-        self.column_null = column_null
+        self.column_not_null = column_not_null
         self.column_default = column_default
-        self.column_comment = column_description
+        self.column_description = column_description
         self.column_range = column_range
         self.column_enum = column_enum
         self.under_supervision = under_supervision
@@ -54,3 +56,33 @@ class ColumnDef:
 
     def __str__(self):
         return f"name:{self.column_name}\ntype:{self.column_type}\nlength:{self.column_length}\nnull:{self.column_null}\ndefault:{self.column_default}\ncomment:{self.column_comment}\nrange:{self.column_range}\nenum:{self.column_enum}\nunder_supervision:{self.under_supervision}\nin_general_ledger:{self.in_general_ledger}\n"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get_column_type(self):
+        if not self.column_type:
+            self.column_type = "varchar"
+            self.column_length = 255
+
+        if self.column_length:
+            return f"{self.column_type}({self.column_length})"
+        else:
+            return self.column_type
+
+    def get_column_null(self):
+        if self.column_not_null:
+            return "NOT NULL"
+        else:
+            return ""
+
+    def get_column_default(self):
+        if self.column_default:
+            return f"DEFAULT {self.column_default}"
+        else:
+            return ""
+
+    def get_column_comment(self):
+        cmt = "COMMENT '"
+        if self.column_description:
+            cmt += f"desc: {self.column_description},\n"
