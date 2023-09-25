7aca7ac9def99e58cde9134fa6cfd5b59ee5b83a
Shang Yehua <niceshang@outlook.com>
2023-09-25 17:05:41 +0800

    add test

--- python/django-lab/mysite/polls/tests.py
+++ python/django-lab/mysite/polls/tests.py
@@ -1,3 +1,57 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
 
 # Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is
+        in the future.
+        """
+
+        # Create a question with a pub_date in the future
+        # (30 days from now).
+        time = timezone.now() + datetime.timedelta(days=30)
+
+        future_question = Question(pub_date=time)
+
+        # Check the output of was_published_recently().
+        # It should be False.
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is
+        older than 1 day.
+        """
+
+        # Create a question with a pub_date in the past
+        # (30 days ago).
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+
+        old_question = Question(pub_date=time)
+
+        # Check the output of was_published_recently().
+        # It should be False.
+        self.assertIs(old_question.was_published_recently(), False)
+        
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date is
+        within the last day.
+        """
+
+        # Create a question with a pub_date within the last day
+        # (30 days ago).
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+
+        recent_question = Question(pub_date=time)
+
+        # Check the output of was_published_recently().
+        # It should be True.
+        self.assertIs(recent_question.was_published_recently(), True)
--- python/django-lab/mysite/polls/views.py
+++ python/django-lab/mysite/polls/views.py
@@ -1,43 +1,55 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,Http404,HttpResponseRedirect
-from .models import Question,Choice
+from django.db.models import F
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
-from django.db.models import F
+from django.utils import timezone
+
+from .models import Choice, Question
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
 
-def detail(request,question_id):
+def index(request):
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
     #     raise Http404("Question does not exist.")
     # return render(request,'polls/detail.html',{'question':question})
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/detail.html',{'question':question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
 
-def vote(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        select_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError,Choice.DoesNotExist):
-        return render(request,'polls/detail.html',{
-            'question':question,
-            'error_message':"You didn't select a choice.",
-        })
+        select_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
     else:
-        select_choice.votes = F('votes') + 1
+        select_choice.votes = F("votes") + 1
         select_choice.save()
         # select_choice.refresh_from_db()
-        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
