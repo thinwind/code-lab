6e7caa57bdfd2d7703d1f87e5630cbecb00ff345
Shang Yehua <niceshang@outlook.com>
2023-09-25 19:28:10 +0800

    add static

--- python/django-lab/mysite/polls/admin.py
+++ python/django-lab/mysite/polls/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Question,Choice
 
-admin.site.register(Question)
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("内容", {"fields": ["question_text"]}),
+        ("时间", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
