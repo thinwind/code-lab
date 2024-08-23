9fa6871aa8f4c77bc38594d83d76dfebeadfe0bc
Shang Yehua <niceshang@outlook.com>
2024-08-23 19:28:03 +0800

    dom4j

--- python/django-lab/mysite/polls/admin.py
+++ python/django-lab/mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -14,6 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ("时间", {"fields": ["pub_date"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
 
 
 admin.site.register(Question, QuestionAdmin)
